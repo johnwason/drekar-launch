51005d28d180d6ac16132d3fef7ecb0cc832960e
John Wason <wason@wasontech.com>
2023-05-31 18:37:59 -0400

    Send sigint to stop Linux tasks

--- simple_launch.py
+++ simple_launch.py
@@ -324,7 +324,7 @@ class SimpleSubprocessImpl:
             import signal
             pid = self._process.pid
             pgid = os.getpgid(pid)
-            os.killpg(pgid, signal.SIGTERM)
+            os.killpg(pgid, signal.SIGINT)
 
     def close(self):
         if sys.platform == "win32":            
